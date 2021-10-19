08992c724dcf9c09c4398fdb065ee2c4546f4ce9
Tarek Ma7mod <Tarek.Mahmoud244@gmail.com>
2021-10-19 16:15:30 +0200

    packages

---
+++ MVC_Teeba_Project/MVC_Teeba_Project/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MVC_Teeba_Project
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/animate.css",
+                      "~/Content/css/icomoon.css",
+                      "~/Content/css/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css/mine").Include(
+                        "~/Content/css/style.css",
+                        "~/Content/css/minestyle.css",
+                        "~/Content/css/mystyle.css"));
+        }
+    }
+}
